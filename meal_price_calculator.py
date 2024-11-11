85ee750eab8df24e421ec265c0a37f41adf11ff6
rol24002 <rol24002@byui.edu>
2024-11-11 11:13:04 -0700

    finish

--- meal_price_calculator.py
+++ meal_price_calculator.py
@@ -1,7 +1,10 @@
 """
         Madison Rollins
         Meal Price Caclculator
-
+            I added a tip funcion and gave the total it's own space.  
+            The tip functions like the sales tax and the two are added to the subtotal to give the new total.
+"""
+""" number of meals
         Ask the user for the following:
             The price of a child's meal (floating point)
             The price of an adult's meal (floating point)
@@ -36,4 +39,67 @@ subtotal = (float(child_price) + float(adult_price))
 print()
 print('-----------')
 #sub- print
-print(f'The Subtotal is {subtotal}')
+print(f'The Subtotal is ${subtotal: .2f}')
+print()
+print('-----------')
+""" Total Calculations
+Ask the user for the sales tax rate as a percentage (floating point). 
+Please note that this percentage should be entered and stored as a number such as 6, or 6.5, not 0.06 or 0.065.
+
+Compute and display the sales tax, by multiplying the subtotal by the sales tax rate divided by 100.
+
+Compute and display the total price of the meal by adding the subtotal and the sales tax.
+"""
+print()
+print('Please Submit the Following:')
+print()
+#ask tax
+tax = input('Enter the sales tax:')
+#math
+sales = ((float(tax)/100) * float(subtotal))
+print()
+#prints
+print(f'Sales Tax: ${sales: .2f}')
+print()
+s
+""" Adding Tip
+"""
+print('-----------')
+print()
+print('Please Submit the Following:')
+print()
+#moolah
+tip_percent = input('Tip percentage:')
+#math
+tip = ((float(tip_percent)/100) * float(subtotal))
+print()
+print(f'Tip: ${tip: .2f}')
+print()
+print('-----------')
+print()
+print()
+""" TOTAL """
+print('-----TOTAL-----')
+#math
+total = (float(sales) + float(subtotal) + float(tip))
+#ptitn
+print()
+print(f'Your Total is ${total: .2f}')
+print()
+""" Change 
+Ask the user for the the payment amount (floating point)
+
+Compute and display the change.
+"""
+print('-----------')
+print()
+print('Please Submit the Following:')
+print()
+#moolah
+bill = input('Total of cash given:')
+#math
+change = (float(bill) - float(total))
+print()
+print(f'Your change is ${change: .2f}')
+print()
+print('-----------')
